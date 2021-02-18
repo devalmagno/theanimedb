c83e4ce7c5bcc7f99a9fd77c8c1f60e43b2e3d19
devalmagno <devalmagno@gmail.com>
2021-02-18 16:55:29 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# theanimedb
---
+++ pages/index.js
@@ -0,0 +1,89 @@
+import Head from 'next/head';
+import { FiSearch } from 'react-icons/fi';
+
+import styles from '../styles/home.module.css';
+
+function HomePage() {
+    return (
+        <div className={styles.content}>
+            <Head>
+                <title>Banco de Dados de Anime</title>
+            </Head>
+            <header className={styles.header}>
+                <h1>Banco de Dados de <strong>Anime</strong></h1>
+                <div className={styles.form}>
+                    <input
+                        type="search"
+                        name="search"
+                        required
+                        placeholder="Procure pelo anime aqui..."
+                        autoComplete="off"
+                    />
+                    <label for="search" className={styles.search_icon}>
+                        <FiSearch color="#C2C2C2" />
+                    </label>
+                    <label for="search" className={styles.search_bottom}></label>
+                </div>
+            </header>
+
+            <main>
+                <div className={styles.sidebar_holder}></div>
+
+                <div className={styles.sidebar}>
+                    <h2>TOP 5 Animes</h2>
+                    <div className={styles.item_top}>
+                        <a>
+                            <figure>
+                                <img src="/images/SNK.jpg" alt="Anime Image" />
+                            </figure>
+                        </a>
+                        <a>Shingeki no Kyojin</a>
+                    </div>
+
+                    <div className={styles.item_sidebar}>
+                        <a>
+                            <figure>
+                                <img src="/images/SNK.jpg" alt="Anime Image" />
+                            </figure>
+                        </a>
+
+                        <a>Shingeki no Kyojin</a>
+                    </div>
+
+                    <div className={styles.item_sidebar}>
+                        <a>
+                            <figure>
+                                <img src="/images/SNK.jpg" alt="Anime Image" />
+                            </figure>
+                        </a>
+
+                        <a>Shingeki no Kyojin</a>
+                    </div>
+
+                    <div className={styles.item_sidebar}>
+                        <a>
+                            <figure>
+                                <img src="/images/SNK.jpg" alt="Anime Image" />
+                            </figure>
+                        </a>
+
+                        <a>Shingeki no Kyojin</a>
+                    </div>
+
+                    <div className={styles.item_sidebar}>
+                        <a>
+                            <figure>
+                                <img src="/images/SNK.jpg" alt="Anime Image" />
+                            </figure>
+                        </a>
+
+                        <a>Shingeki no Kyojin</a>
+                    </div>
+                </div>
+                <div className={styles.sidebar_holder}></div>
+            </main>
+        </div>
+    );
+}
+
+export default HomePage;
